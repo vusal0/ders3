531929df9395cf07cce09657b52a82e4108cc9b3
Nubar Sariyeva <7nme01l@code.edu.az>
2023-04-12 08:39:57 -0700

    ders

---
+++ main.js
@@ -0,0 +1,73 @@
+// var num= promt();
+
+// let teklik = num % 10;
+// let onluq =(num - teklik) / 10
+
+// if (num > 9 && num<100) {
+//     if (onluq != teklik){
+//         alert ("ededin reqemleri ferqlidir");
+//     }
+//     else {
+//         alert ("ededin reqemleri ferqli deyil")
+//     }
+// }
+
+// else {
+    alert ("T")
+// }
+
+// var mount = prompt();
+
+//  if (mount == 'yanvar') {
+//      alert("31");
+// }
+// else if (mount == 'fevral') {
+//      alert("27");
+// }
+// else if (mount == 'mart') {
+//     alert("31");
+// }
+// else if (mount == 'aprel') {
+//     alert("30");
+// }
+// else if (mount == 'may') {
+//     alert("31");
+// }
+//  else if (mount == 'iyun') {
+//     alert("30");
+// }
+// else if (mount == 'iyul') {
+//     alert("31");
+// }
+// else {
+//    alert("Wrong input");
+// }
+
+// var mount = prompt();
+// switch (mount) {
+//         case "yanvar":
+//             alert("31");
+//             break;
+//         case "fevral":
+//             alert("27");
+//             break;
+//         case "mart":
+//             alert("31");
+//             break;
+//         case "aprel":
+//             alert("30");
+//             break;
+//         case "may":
+//             alert("31");
+//             break;
+//         case "iyun":
+//             alert("30");
+//             break;
+//         default:
+//             alert("Wrong input");
+//             break;
+//     }
+
+var num = prompt();
+
+let 
